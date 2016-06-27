6b6dd8d0263ed51d921e5ff72286db69ed4b8bbe
Connor <tevancollins@gmail.com>
2016-06-26 21:40:39 -0600

    Initial commit

--- main/src/Network.java
+++ main/src/Network.java
@@ -1,6 +1,4 @@
-import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
--- main/src/Neuron.java
+++ main/src/Neuron.java
@@ -1,7 +1,5 @@
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.ListIterator;
 
 /**
  * Created by Connor on 6/26/2016.
